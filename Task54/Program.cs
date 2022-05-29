56bbdd73759596a81ff0d3a117c961a52be23e9e
Antonio <don_ne@mail.ru>
2022-05-29 14:28:21 +0400

    Task54-60

---
+++ Task54/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+using System;
+Console.Clear();
+
+int m = ReadInteger("Введите количество строк m=");
+int n = ReadInteger("Введите количество столбцов n=");
+int minimum = -99;
+int maximum = 99;
+
+int[,] array = CreateArrayRandom(m, n, minimum, maximum);
+Console.WriteLine("Массив:");
+PrintArray(array);
+
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+        for (int k = j + 1; k < array.GetLength(1); k++)
+        {
+            if (array[i, k] > array[i, j])
+            {
+                int temp = array[i, j];
+                array[i, j] = array[i, k];
+                array[i, k] = temp;
+            }
+        }
+
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив:");
+PrintArray(array);
+
+int[,] CreateArrayRandom(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+
+    return array;
+}
+
+int ReadInteger(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
---
+++ Task58/Program.cs
@@ -0,0 +1,68 @@
+﻿//Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+using System;
+Console.Clear();
+
+int[,] firstArray = GetFirstArray(5, 5, 1, 9);
+int[,] secondArray = GetSecondArray(5, 5, 1, 9);
+PrintArray(firstArray);
+Console.WriteLine();
+PrintArray(secondArray);
+Console.WriteLine();
+PrintArray(GetMultyArray(firstArray, secondArray));
+
+
+
+int[,] GetFirstArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] firstResult = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            firstResult[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return firstResult;
+}
+
+int[,] GetSecondArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] secondResult = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            secondResult[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return secondResult;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] GetMultyArray(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] multyArray = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0 ; i < firstMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondMatrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < secondMatrix.GetLength(0); k++)
+            {
+                multyArray[i, j] += firstMatrix[i, k] * secondMatrix[k, j];
+            }
+        }
+    }
+    return multyArray;
+}
